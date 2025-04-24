b942951e73e4559c457b5f11a181cbda99b810c6
Will IM <vitalii.mieliet@gmail.com>
2025-04-24 22:56:06 +0300

    Do tasks

--- js/task-2.js
+++ js/task-2.js
@@ -15,6 +15,29 @@ removeItem(itemToRemove) — приймає рядок з назвою това�
 'use strict';
 console.log('\nTask-2\n\n');
 
+class Storage {
+  #items;
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
+}
+
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
